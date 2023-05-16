02492fe9951be83c3457f8e96193627b00f7c0f8
Nikolay <kolyaa2010@mail.ru>
2023-05-16 17:23:01 +0700

    Creating a project

---
+++ src/components/app/App.tsx
@@ -0,0 +1,13 @@
+import ProductCard from "../productCard/ProductCard"
+
+import "./app.scss";
+
+function App() {
+  return (
+    <div>
+      <ProductCard/>
+    </div>
+  );
+}
+
+export default App;
